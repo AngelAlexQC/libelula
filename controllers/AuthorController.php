163f118fbecb3a09af08f70be20bd58eaa1d3b8b
Ángel Quiroz <aquirozdev@gmail.com>
2023-10-25 23:21:20 -0500

    feat: initial swagger

--- controllers/AuthorController.php
+++ controllers/AuthorController.php
@@ -4,7 +4,118 @@ namespace app\controllers;
 
 use yii\rest\ActiveController;
 
+/**
+ * {
+    "name": "J. R. R. Tolkien",
+    "birth_date": "1892-01-03",
+    "country": "United Kingdom"
+}
+ */
 class AuthorController extends ActiveController
 {
     public $modelClass = 'app\models\Author';
+
+    /**
+     * @OA\Get(path="/authors",
+     *   summary="Obtener todos los autores",
+     *   tags={"autores"},
+     *   @OA\Response(response=200, description="Obtener todos los autores")
+     * )
+     */
+    public function actionIndex()
+    {
+        return parent::actions();
+    }
+
+    /**
+     * @OA\Post(path="/authors",
+     *   summary="Crear un autor",
+     *   tags={"autores"},
+     *   @OA\Response(response=200, description="Crear un autor"),
+     *   @OA\RequestBody(
+     *    @OA\MediaType(
+     *     mediaType="application/json",    
+     *       @OA\Schema(
+     *         @OA\Property(property="name", type="string", example="J. R. R. Tolkien"),
+     *         @OA\Property(property="birth_date", type="string", example="1892-01-03"),
+     *         @OA\Property(property="country", type="string", example="United Kingdom"),
+     *       )
+     *   ),
+     *  )
+     * )
+     */
+    public function actionCreate()
+    {
+        return parent::actions();
+    }
+
+    /**
+     * @OA\Get(path="/author/view",
+     *   summary="Obtener un autor por su ID",
+     *   tags={"autores"},
+     *   @OA\Response(response=200, description="Obtener un autor por su ID"),
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="query",
+     *     required=true,
+     *     @OA\Schema(
+     *         type="string"
+     *    )
+     *  )
+     * )
+     */
+    public function actionView($id)
+    {
+        return parent::actions();
+    }
+
+    /**
+     * @OA\Put(path="/author/update",
+     *   summary="Actualizar un autor por su ID",
+     *   tags={"autores"},
+     *   @OA\Response(response=200, description="Actualizar un autor por su ID"),
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="query",
+     *     required=true,
+     *     @OA\Schema(
+     *         type="string"
+     *    )
+     *  ),
+     *   @OA\RequestBody(
+     *    @OA\MediaType(
+     *     mediaType="application/json",    
+     *       @OA\Schema(
+     *         @OA\Property(property="name", type="string", example="J. R. R. Tolkien"),
+     *         @OA\Property(property="birth_date", type="string", example="1892-01-03"),
+     *         @OA\Property(property="country", type="string", example="United Kingdom"),
+     *       )
+     *   ),
+     *  )
+     * )
+     */
+    public function actionUpdate($id)
+    {
+        return parent::actions();
+    }
+
+    /**
+     * @OA\Delete(path="/author/delete",
+     *   summary="Eliminar un autor por su ID",
+     *   tags={"autores"},
+     *   @OA\Response(response=200, description="Eliminar un autor por su ID"),
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="query",
+     *     required=true,
+     *     @OA\Schema(
+     *         type="string"
+     *    )
+     *  )
+     * )
+     */
+    public function actionDelete($id)
+    {
+        return parent::actions();
+    }
 }
--- controllers/BookController.php
+++ controllers/BookController.php
@@ -4,7 +4,118 @@ namespace app\controllers;
 
 use yii\rest\ActiveController;
 
+
+/**
+ * @OA\Info(title="LibelulaSoft Test", version="0.1")
+ * @OA\PathItem(path="/books")
+ */
 class BookController extends ActiveController
 {
     public $modelClass = 'app\models\Book';
+
+    /**
+     * @OA\Get(path="/books",
+     *   summary="Obtener todos los libros",
+     *   tags={"libros"},
+     *   @OA\Response(response=200, description="Obtener todos los libros")
+     * )
+     */
+    public function actionIndex()
+    {
+        return parent::actions();
+    }
+
+    /**
+     * @OA\Post(path="/books",
+     *   summary="Crear un libro",
+     *   tags={"libros"},
+     *   @OA\Response(response=200, description="Crear un libro"),
+     *   @OA\RequestBody(
+     *    @OA\MediaType(
+     *     mediaType="application/json",    
+     *       @OA\Schema(
+     *         @OA\Property(property="title", type="string", example="The Lord of the Rings"),
+     *         @OA\Property(property="author", type="string", example="6539d81db2638d07a20df3f7"),
+     *         @OA\Property(property="year", type="integer", example="1954"),
+     *         @OA\Property(property="description", type="string", example="The Lord of the Rings is an epic high-fantasy novel written by English author and scholar J. R. R. Tolkien."),
+     *       )
+     *   ),
+     *  )
+     * )
+     */
+    public function actionCreate()
+    {
+        return parent::actions();
+    }
+
+    /**
+     * @OA\Get(path="/book/view",
+     *   summary="Obtener un libro por su ID",
+     *   tags={"libros"},
+     *   @OA\Response(response=200, description="Obtener un libro por su ID"),
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="query",
+     *     required=true,
+     *     @OA\Schema(
+     *         type="string"
+     *     )
+     *   )
+     * )
+     */
+    public function actionView($id)
+    {
+        return parent::actions();
+    }
+
+    /**
+     * @OA\Patch(path="/book/update",
+     *   summary="Actualizar un libro por su ID",
+     *   tags={"libros"},
+     *   @OA\Response(response=200, description="Actualizar un libro por su ID"),
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="query",
+     *     required=true,
+     *     @OA\Schema(
+     *         type="string"
+     *     )
+     *   ),
+     *   @OA\RequestBody(
+     *    @OA\MediaType(
+     *     mediaType="application/json",    
+     *       @OA\Schema(
+     *         @OA\Property(property="title", type="string", example="El señor de los anillos"),
+     *         @OA\Property(property="author", type="string", example="6539d81db2638d07a20df3f7"),
+     *         @OA\Property(property="year", type="integer", example="1954"),
+     *         @OA\Property(property="description", type="string", example="El señor de los anillos es una novela de fantasía épica escrita por el filólogo y escritor británico J. R. R. Tolkien."),
+     *       )
+     *   ),
+     *  )
+     * )
+     */
+    public function actionUpdate($id)
+    {
+        return parent::actions();
+    }
+
+    /**
+     * @OA\Delete(path="/book/delete",
+     *   summary="Eliminar un libro por su ID",
+     *   tags={"libros"},
+     *   @OA\Response(response=200, description="Eliminar un libro por su ID"),
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="query",
+     *     required=true,
+     *     @OA\Schema(
+     *         type="string"
+     *     ),
+     *   )
+     * )
+     */
+    public function actionDelete($id)
+    {
+        return parent::actions();
+    }
 }
